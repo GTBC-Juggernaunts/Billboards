3a35e0ef05bf47338f9dc706a4a3f994f66dac77
D.Biles <darron.biles@gmail.com>
2019-01-22 23:56:40 -0500

    [WIP] - Compartment files

---
+++ example/components/screens/Home/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+class Home extends Component {
+    render() {
+        return
+        <View>
+            <Text>This is the Home Screen</Text>
+        </View>
+    }
+};
+
+export default Home;
---
+++ example/components/style/style.js
@@ -0,0 +1,25 @@
+import {
+    StyleSheet
+} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#AAC0AA"
+    },
+    welcome: {
+        flex: 2,
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5
+    }
+});
+
+export default styles;
