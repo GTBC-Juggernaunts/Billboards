5884ce780235537d0ddd2d91054ea0f4d029becb
Peter Huynh <huynhxpeter@gmail.com>
2019-02-05 21:15:33 -0500

    feat: add tab icons

--- mobile/components/Navigator/navigator.js
+++ mobile/components/Navigator/navigator.js
@@ -1,53 +1,74 @@
+import React from 'react';
 import {
-    createBottomTabNavigator,
-    createStackNavigator,
-    createAppContainer
+  createBottomTabNavigator,
+  createStackNavigator,
+  createAppContainer
 } from 'react-navigation';
-import { Icon } from 'react-native-elements'
-import Home from '../screens/home/home'
-import Register from '../screens/home/register'
-import Beacon from '../screens/coupons/beacons'
-
+import { Icon } from 'react-native-elements';
+import Home from '../screens/home/home';
+import Register from '../screens/home/register';
+import Beacon from '../screens/coupons/beacons';
 
 // Screen within Tabs
-const homeStack = createStackNavigator({
+const homeStack = createStackNavigator(
+  {
     Home: {
-        screen: Home,
+      screen: Home
     },
     Register: {
-        screen: Register,
-    },
-}, {
-    initialRouteName: 'Home',
-    headerMode: "none",
-});
-
-const couponStack = createStackNavigator({
-    Coupons: {
-        screen: Beacon,
-        navigationOptions: () => ({
-            tabBarIcon: () => {
-                <Icon name="shopping-bag" size={20} />
-            }
-        })
+      screen: Register
     }
-}, {
-    headerMode: "none"
-});
+  },
+  {
+    initialRouteName: 'Home',
+    headerMode: 'none'
+  }
+);
+
+const couponStack = createStackNavigator(
+  {
+    Promotions: {
+      screen: Beacon
+    }
+  },
+  {
+    headerMode: 'none'
+  }
+);
 
 // Tab menu at bottom of the screen
-const AppNavigator = createAppContainer(createBottomTabNavigator({
-        Home: homeStack,
-        Coupons: couponStack,
+const AppNavigator = createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: {
+        screen: homeStack,
+        navigationOptions: () => ({
+          tabBarIcon: ({ tintColor }) => (
+            <Icon name="home" type="font-awesome" size={20} color={tintColor} />
+          )
+        })
+      },
+      Promotions: {
+        screen: couponStack,
+        navigationOptions: () => ({
+          tabBarIcon: ({ tintColor }) => (
+            <Icon
+              name="shopping-bag"
+              type="font-awesome"
+              size={20}
+              color={tintColor}
+            />
+          )
+        })
+      }
     },
-        {
-            swipeEnabled: false, 
-            tabBarOptions: {
-                showIcon: true,
-                activeTintColor: 'red',
-            },
-        }
-    )
+    {
+      swipeEnabled: false,
+      tabBarOptions: {
+        activeTintColor: 'red'
+      }
+    }
+  )
 );
 
 export default AppNavigator;
